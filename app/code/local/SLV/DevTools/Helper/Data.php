6d42252678431862ed4d52904d40266e915746f3
Sergey Petrov <sergoslav@gmail.com>
2014-10-08 23:41:52 +0400

    first commit

---
+++ app/code/local/SLV/DevTools/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergoslav
+ * Date: 08.10.14
+ * Time: 23:25
+ */ 
+class SLV_DevTools_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/local/SLV/DevTools/controllers/OrderController.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Class SnowCommerce_Develop_OrderController
+ *
+ * Develop Order Controller
+ */
+class SnowCommerce_Develop_OrderController extends Mage_Core_Controller_Front_Action
+{
+
+    protected function _getProductId()
+    {
+        $productsId = array(10507,10508,10509,10510,10511,10512);
+
+        return $productsId[array_rand($productsId)];
+    }
+
+    protected function _getCustomer()
+    {
+        $customersId = array(1,10,4,5,8);
+        $customerId = $customersId[array_rand($customersId)];
+
+        return Mage::getModel('customer/customer')->load($customerId);
+    }
+
+    public function placeorderAction()
+    {
+        $customer = $this->_getCustomer();
+
+        $quote = Mage::getModel('sales/quote')
+            ->setStoreId(Mage::app()->getStore()->getId()) // will be used current store
+            ->assignCustomer($customer);
+
+        $product = Mage::getModel('catalog/product')->load($this->_getProductId());
+
+
+        $item = $quote->addProduct($product, new Varien_Object(array(
+            'product'         => $product->getId(),
+            'qty'             => 1,
+        )));
+
+        if(!is_object($item)) throw new Exception('something wrong with product');
+
+        // save quotes
+        $quote->collectTotals()->save();
+
+        $billingAddress = Mage::getSingleton('sales/quote_address')->importCustomerAddress(
+            //Mage::getModel('customer/address')->load($customer->getDefaultBilling())
+            $customer->getDefaultBillingAddress()
+        );
+
+        if($billingAddress->getId())
+        {
+            $quote->setBillingAddress($billingAddress);
+        }
+//        else
+//        {
+//            $quote->getBillingAddress()->addData($this->getRequest()->getParams());
+//        }
+
+
+        $shippingAddress = Mage::getSingleton('sales/quote_address')->importCustomerAddress(
+            //Mage::getModel('customer/address')->load($customer->getDefaultShipping())
+            $customer->getDefaultShippingAddress()
+        );
+
+        if($shippingAddress->getId())
+        {
+            $quote->setShippingAddress($shippingAddress);
+        }
+//        $quote->getShippingAddress()->addData($this->getRequest()->getParams());
+
+        $quote->collectTotals();
+
+        $quote->getShippingAddress()->setPaymentMethod('checkmo');
+        $quote->getShippingAddress()->setCollectShippingRates(true)->collectShippingRates()
+            ->setShippingMethod('freeshipping_freeshipping'); // set shipping method
+
+        $quote->getPayment()->importData(array('method' => 'checkmo')); // set payment method
+        $quote->setTotalsCollectedFlag(false)->collectTotals()->save();
+
+        try
+        {
+            $quote->setSendCconfirmation(true);
+            $service = Mage::getModel('sales/service_quote', $quote);
+            $service->submitAll();
+            $order = $service->getOrder();
+            $order->sendNewOrderEmail();
+            $quote
+                ->setIsActive(false)
+                ->save();
+
+            return $this->_redirect('checkout/onepage/success');
+        }
+        catch(Exception $e)
+        {
+            Mage::log($e->getMessage(), null, "slv_devtool_placeorder.log");
+            echo $e->getMessage();
+
+            die();
+//            return $this->_redirect('');
+        }
+    }
+}
